a5c0f4f11554e686e649e2d263061d90344fdee2
David Wolf <68775926+devidw@users.noreply.github.com>
2022-02-17 17:25:06 +0100

    Add image creation helper

--- src/Helper.php
+++ src/Helper.php
@@ -23,4 +23,44 @@ class Helper
         );
         return file_put_contents($theme_file, $theme_data) !== false;
     }
+
+    /**
+     * Image binary data into a WordPress attachment.
+     * 
+     * @see https://gist.github.com/hissy/7352933
+     * 
+     * @param string $filename The name of the file.
+     * @param string $binaries The binary data of the image.
+     * 
+     * @return int|null The attachment ID.
+     */
+    public static function createAttachmentFromImage(string $filename, string $binaries): ?int
+    {
+        $file = wp_upload_bits($filename, null, $binaries);
+
+        if ($file instanceof WP_Error) {
+            return null;
+        }
+
+        $fileType = wp_check_filetype($file['file'], null);
+
+        $attachment = [
+            'post_mime_type' => $fileType['type'],
+            'post_title' => $filename,
+        ];
+
+        $attachmentId = wp_insert_attachment($attachment, $file['file']);
+
+        if ($attachmentId instanceof WP_Error) {
+            return null;
+        }
+
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+        $attachmentData = wp_generate_attachment_metadata($attachmentId, $file['file']);
+
+        wp_update_attachment_metadata($attachmentId,  $attachmentData);
+
+        return $attachmentId;
+    }
 }
